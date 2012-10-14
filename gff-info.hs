15ad955cf8d758b703ac6cc3ea17e18b076b64ae
David Powell <david@drp.id.au>
2012-10-15 10:43:21 +1100

    Use a Bytestring for representing DNA

--- gff-info.hs
+++ gff-info.hs
@@ -15,6 +15,7 @@ import Data.Maybe
 import Data.List
 import Data.List.Split
 import qualified Data.Map as M
+import qualified Data.ByteString.Char8 as BS
 import Debug.Trace
 import Text.Printf
 import Text.Regex.PCRE
@@ -22,6 +23,8 @@ import Control.Monad.State
 import System.Console.CmdArgs hiding (typ)
 import System.IO
 import CmdArgs_zsh
+
+type Seq = BS.ByteString
 
 data Options = Stats     { file :: Maybe FilePath }
              | CDS_Check { file :: Maybe FilePath }
@@ -54,14 +57,13 @@ programOptions =
     &= help "gff-info may be used to display some statistics about a GFF file.\n\
             \Alternatively, it may also perform some processing on a GFF file\n\
             \such as removing certain contigs, or setting new IDs on all the features\n\
-            \Currently supports GFF3 with sequences inline as '##DNA' or after '##FASTA'\n\
-            \"
+            \Currently supports GFF3 with sequences inline as '##DNA' or after '##FASTA'\n"
 
 data GFF = GFF { scaffolds :: [Scaffold]
                , features :: [Feature]
                }
 
-data Scaffold = Scaffold { s_name :: String, dna :: String }
+data Scaffold = Scaffold { s_name :: String, dna :: Seq }
               deriving (Show)
 
 data Feature = Feature { seq_id, source, typ :: String
@@ -102,13 +104,13 @@ setFeatAttrib k f v = f { attributes = M.insert k [v] (attributes f) }
 parseScaffold :: [String] -> (Scaffold, [String])
 parseScaffold (l:ls) = let name = tail (dropWhile (' ' /=) l)
                            (dnaLines,rest) = break ("##end-DNA" `isPrefixOf`) ls
-                       in (Scaffold {s_name = name, dna = concatMap (drop 2) dnaLines}
+                       in (Scaffold {s_name = name, dna = BS.pack $ concatMap (drop 2) dnaLines}
                           , rest)
 parseFastaScaffolds :: [String] -> [Scaffold]
 parseFastaScaffolds ls
     | null ls = []
     | ">" `isPrefixOf` head ls = let (dna,rest) = break (">" `isPrefixOf`) (tail ls)
-                                 in Scaffold {s_name = tail (head ls), dna = concat dna}
+                                 in Scaffold {s_name = tail (head ls), dna = BS.pack $ concat dna}
                                         : parseFastaScaffolds rest
     | otherwise = error $ "Bad FASTA section : "++show (take 5 ls)
 
@@ -126,10 +128,10 @@ parseLines lss@(l:ls)
 getScaffold :: GFF -> String -> Maybe Scaffold
 getScaffold gff name = lookup name $ map (\s -> (s_name s, s)) (scaffolds gff)
 
-featSeq :: GFF -> Feature -> String
+featSeq :: GFF -> Feature -> Seq
 featSeq gff f = case getScaffold gff (seq_id f) of
-                  Nothing -> error "No scaffold by the name : "++seq_id f
-                  Just sc -> take (featLen f) . drop (start f - 1) . dna $ sc
+                  Nothing -> error $ "No scaffold by the name : "++seq_id f
+                  Just sc -> BS.take (featLen f) . BS.drop (start f - 1) . dna $ sc
 
 -- | TODO, should check ##gff-version
 parseGff :: [String] -> GFF
@@ -148,9 +150,9 @@ gffOutput asFasta gff = ["##gff-version 3"]
                            ++ concatMap scaffoldAsFasta (scaffolds gff)
                | otherwise = []
     scaffoldOutput s = [printf "##DNA %s" (s_name s)]
-                       ++ map ("##"++) (toChunks 40 $ dna s)
+                       ++ map ("##"++) (toChunks 40 . BS.unpack $ dna s)
                        ++ ["##end-DNA"]
-    scaffoldAsFasta s = (">"++s_name s) : (toChunks 60 $ dna s)
+    scaffoldAsFasta s = (">"++s_name s) : (toChunks 60 . BS.unpack $ dna s)
 
 toChunks _ [] = []
 toChunks n s = let (a,b) = splitAt n s
@@ -199,12 +201,12 @@ prStats gff = do
   let avgCDS = avg totCDS (length joinedCDS)
   printf "Avg joined CDS length = %.1f (%.1faa)\n" avgCDS (avgCDS / 3)
 
-  printf "Coding = %.1f%%\n" (100 * avg totCDS (sum . map (length . dna) . scaffolds $ gff))
+  printf "Coding = %.1f%%\n" (100 * avg totCDS (sum . map (BS.length . dna) . scaffolds $ gff))
 
   -- Tot info
   putStrLn "Total : "
-  putStr "  " >> prSummary (map (length . dna) . scaffolds $ gff)
-  putStr "  " >> prSeqStats (concatMap dna . scaffolds $ gff)
+  putStr "  " >> prSummary (map (BS.length . dna) . scaffolds $ gff)
+  putStr "  " >> prSeqStats (BS.concat . map dna . scaffolds $ gff)
 
   printf "Num scaffolds = %s\n" (formatInt . length . scaffolds $ gff)
 
@@ -223,7 +225,7 @@ prFeatStats :: GFF -> [Feature] -> IO ()
 prFeatStats gff fs = do
   let featLens = map featLen fs
   putStr "  " >> prSummary featLens
-  putStr "  " >> prSeqStats (concatMap (featSeq gff) fs)
+  putStr "  " >> prSeqStats (BS.concat . map (featSeq gff) $ fs)
 
 prSummary :: [Int] -> IO ()
 prSummary nums =
@@ -232,11 +234,12 @@ prSummary nums =
   in printf "Num=%s avg=%.1fbp min=%sbp max=%sbp n50=%sbp\n" (formatInt $ length nums)
                (avg (sum nums) (length nums)) (formatInt $ minimum nums) (formatInt $ maximum nums) (formatInt n50)
 
-prSeqStats :: String -> IO ()
+prSeqStats :: Seq -> IO ()
 prSeqStats dna = do
-  let counts = M.assocs $ M.fromListWith (+) $ map (,1) dna
+  -- let counts = M.assocs $ M.fromListWith (+) $ map (,1) dna
+  let counts = M.assocs $ BS.foldl (\mp c -> M.insertWith' (+) c 1 mp) M.empty dna
   -- print counts
-  let tot = sum $ map snd counts
+  let tot = BS.length dna
   let [a,t,g,c,n] = map (\c -> fromMaybe (0::Int) . lookup c $ counts) "ATGCN"
   printf "Total=%sbp Ns=%d G+C=%.1f%%\n" (formatInt tot) n (100 * avg (g+c) (a+t+g+c))
 
